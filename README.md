9e5a90555386c2f92a9d01cc4004f5e919c33c1f
tkishel <tkishel@gmail.com>
2017-04-28 09:45:40 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# system_gem
+
+#### Table of Contents
+
+1. [Description](#description)
+1. [Usage - Configuration options and additional functionality](#usage)
+1. [Reference - An under-the-hood peek at what the module is doing and how](#reference)
+
+## Description
+
+This a workaround for PUP-6134:
+
+Unable to install a Ruby gem to non-Puppet Ruby stack using the package resource type on Windows
+
+## Usage
+
+To use this package provider, specify it as the provider of a package.
+
+~~~
+package { 'json':
+  ensure   => present,
+  provider => system_gem,
+}
+~~~
+
+## Reference
+
+Puppet is unable to install a ruby gem anywhere other than Puppet's ruby via the gem package provider on Windows.
+This is caused by Puppet prepending its paths (including the path to Puppet's ruby gem command) to PATH.
+This workaround removes Puppet's paths from PATH when setting :gemcmd.
+
+While this package provider is meant as a workaround for an issue specific to Windows, 
+it is compatible with other operating systems, as it is a child of the gem package provider,
+and only overrides the setting of :gemcmd on Windows.
---
+++ lib/puppet/provider/package/system_gem.rb
@@ -0,0 +1,24 @@
+require 'puppet/provider/package'
+
+Puppet::Type.type(:package).provide :system_gem, :parent => :gem do
+  desc "Puppet System Ruby Gem support, workaround for PUP-6134"
+
+  has_feature :versionable, :install_options, :uninstall_options
+
+  if Puppet.features.microsoft_windows?
+
+    self.debug "Using the system_gem package provider as a workaround for PUP-6134"
+
+    # Puppet on Windows prepends its paths to PATH. One of those paths includes Puppet's ruby gem command.
+    # That results in the which() method in self.command(name) in lib/puppet/provider.rb always returning Puppet's ruby gem command.
+    # To prevent this, call which("gem") with a PATH without Puppet's paths, and set :gemcmd to the full-path result. 
+
+    puppet_paths = 'Puppet Labs'
+    path_array = Puppet::Util.get_env("PATH").split(File::PATH_SEPARATOR)
+    path_array.reject! {|p| p =~ /#{puppet_paths}/}
+    path_without_puppet_paths = path_array.join(File::PATH_SEPARATOR)
+    commands :gemcmd => Puppet::Util.withenv(:PATH => path_without_puppet_paths) { which("gem") }
+  else
+    commands :gemcmd => "gem"
+  end
+end
