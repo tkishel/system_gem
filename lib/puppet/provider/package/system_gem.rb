5dea23bc3fb96c3b118251b2993beb9dcb6e9e2e
tkishel <tkishel@gmail.com>
2017-06-26 12:54:50 -0700

    transparent use outside of windows

--- lib/puppet/provider/package/system_gem.rb
+++ lib/puppet/provider/package/system_gem.rb
@@ -11,9 +11,11 @@ Puppet::Type.type(:package).provide :system_gem, :parent => :gem do
   # That results in the which() method in self.command(name) in lib/puppet/provider.rb always returning Puppet's ruby 'gem' command.
   # To prevent this, call which('gem') with a PATH without Puppet's RUBY_DIR, and set :gemcmd to the full-path result.
 
-  ruby_bin_dir = Puppet::Util.get_env('RUBY_DIR') + '\bin'
-  path_array = Puppet::Util.get_env('PATH').split(';')
-  path_array.reject! {|p| p == ruby_bin_dir}
-  path_without_puppet_ruby_bin_dir = path_array.join(';')
-  commands :gemcmd => Puppet::Util.withenv({:PATH => path_without_puppet_ruby_bin_dir}, :windows) { which('gem') }
+  if Puppet.features.microsoft_windows?
+    ruby_bin_dir = Puppet::Util.get_env('RUBY_DIR') + '\bin'
+    path_array = Puppet::Util.get_env('PATH').split(';')
+    path_array.reject! {|p| p == ruby_bin_dir}
+    path_without_puppet_ruby_bin_dir = path_array.join(';')
+    commands :gemcmd => Puppet::Util.withenv({:PATH => path_without_puppet_ruby_bin_dir}, :windows) { which('gem') }
+  end
 end
