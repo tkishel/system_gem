c0f20a483740b6279f55ff70815b76ecf5fc472a
tkishel <tkishel@gmail.com>
2017-12-29 09:49:18 -0800

    simplify variable names

--- lib/puppet/provider/package/system_gem.rb
+++ lib/puppet/provider/package/system_gem.rb
@@ -11,15 +11,15 @@ Puppet::Type.type(:package).provide :system_gem, :parent => :gem do
     # Puppet on Windows prepends its paths to PATH. One of those paths includes Puppet's RUBY_DIR.
     # That results in the which() method in self.command(name) in lib/puppet/provider.rb always returning Puppet's ruby 'gem' command.
     # To prevent this, call which('gem') with a PATH without Puppet's RUBY_DIR, and set :gemcmd to the full-path result.
-    puppet_ruby_dir = Puppet::Util.get_env('RUBY_DIR')
-	if puppet_ruby_dir.nil?
+    env_ruby_dir = Puppet::Util.get_env('RUBY_DIR')
+	if env_ruby_dir.nil?
 	  commands :gemcmd => "gem"
 	else
-      puppet_ruby_bin_dir = puppet_ruby_dir + '\bin'
-      path_array = Puppet::Util.get_env('PATH').split(';')
-      path_array.reject! {|p| p == puppet_ruby_bin_dir}
-      path_without_puppet_ruby_bin_dir = path_array.join(';')
-      commands :gemcmd => Puppet::Util.withenv({:PATH => path_without_puppet_ruby_bin_dir}, :windows) { which('gem') }
+      puppet_ruby_bin_path = env_ruby_dir + '\bin'
+      path_as_array = Puppet::Util.get_env('PATH').split(';')
+      path_as_array.reject! {|p| p == puppet_ruby_bin_path}
+      path_without_path = path_as_array.join(';')
+      commands :gemcmd => Puppet::Util.withenv({:PATH => path_without_path}, :windows) { which('gem') }
 	end
   else
     commands :gemcmd => "gem"
